b5b4fa9bac9e3910e273dc0370cf04902782ac5d
Franco-009 <fdomingueztupa@mail.austral.edu.ar>
2025-08-25 12:48:59 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,3 +3,9 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+# Ejercicio 3 - Uso del slice
+
+    texto = input()
+    resultado = texto[4::2]
+    print(resultado)
+slice_advanced()
